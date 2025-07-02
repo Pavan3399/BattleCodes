d4367c5309c2cd61763ec9415c0fed6b303df567
Pavan3399 <84954792+Pavan3399@users.noreply.github.com>
2025-07-02 18:25:45 -0400

    Integer_To_Roman.py

---
+++ Integer_To_Roman.py
@@ -0,0 +1,34 @@
+"""#Input: num = 3749
+#Output: "MMMDCCXLIX"
+#Explanation:
+3000 = MMM as 1000 (M) + 1000 (M) + 1000 (M)
+ 700 = DCC as 500 (D) + 100 (C) + 100 (C)
+  40 = XL as 10 (X) less of 50 (L)
+   9 = IX as 1 (I) less of 10 (X)
+Note: 49 is not 1 (I) less of 50 (L) because the conversion is based on decimal places"""
+
+
+class Solution:
+    def intToRoman(self, num):
+        val_to_roman = [
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I"),
+        ]
+        
+        result = ""
+        for value, roman in val_to_roman:
+            while num >= value:
+                result += roman
+                num -= value
+        return result
