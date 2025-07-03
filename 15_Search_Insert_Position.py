14e272e9c53df30be492956580601df0cd5252e3
Pavan3399 <84954792+Pavan3399@users.noreply.github.com>
2025-07-03 11:53:50 -0400

    Create 15_Search_Insert_Position.py

---
+++ 15_Search_Insert_Position.py
@@ -0,0 +1,25 @@
+LeetCode Result:- https://leetcode.com/problems/search-insert-position/submissions/1685203523
+
+
+Code starts
+_____________________________
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        # if not found, left is the insert position
+        return left
