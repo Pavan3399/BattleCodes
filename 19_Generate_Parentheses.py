d2ec9b386c26ee5e088b08b55ff35cafd3f4c587
Pavan3399 <84954792+Pavan3399@users.noreply.github.com>
2025-07-03 12:13:25 -0400

    Create 19_Generate_Parentheses.py

---
+++ 19_Generate_Parentheses.py
@@ -0,0 +1,32 @@
+Leetcode Result:- https://leetcode.com/problems/generate-parentheses/submissions/1685226993
+
+"""Example 1:
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+Example 2:
+Input: n = 1
+Output: ["()"]"""
+
+code start
+___________________________
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        res = []
+
+        def backtrack(s='', left=0, right=0):
+            if len(s) == 2 * n:
+                res.append(s)
+                return
+            if left < n:
+                backtrack(s + '(', left + 1, right)
+            if right < left:
+                backtrack(s + ')', left, right + 1)
+
+        backtrack()
+        return res
+        
