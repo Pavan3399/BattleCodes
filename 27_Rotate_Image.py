25ef8097ffa0e68d72f443494e17fc568c358529
Pavan3399 <84954792+Pavan3399@users.noreply.github.com>
2025-07-06 16:44:45 -0400

    Create 27_Rotate_Image.py

---
+++ 27_Rotate_Image.py
@@ -0,0 +1,24 @@
+Leetcode Results:- https://leetcode.com/problems/rotate-image/submissions/1688850476
+
+Example 1:
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+Code
+_____________________________________
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+
+        # Step 1: Transpose
+        for i in range(n):
+            for j in range(i+1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        # Step 2: Reverse each row
+        for row in matrix:
+            row.reverse()
