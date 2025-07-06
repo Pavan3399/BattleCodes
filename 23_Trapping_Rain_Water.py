ec623869cc1a93c5bfa60e22cfcf4c7c6e7b32cf
Pavan3399 <84954792+Pavan3399@users.noreply.github.com>
2025-07-06 16:22:54 -0400

    Create 23_Trapping_Rain_Water.py

---
+++ 23_Trapping_Rain_Water.py
@@ -0,0 +1,37 @@
+Leetcode Result:- https://leetcode.com/problems/trapping-rain-water/submissions/1688837131
+
+Example 1:
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+
+Code
+______________________
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        if not height:
+            return 0
+
+        left, right = 0, len(height) - 1
+        left_max, right_max = 0, 0
+        water = 0
+
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] >= left_max:
+                    left_max = height[left]
+                else:
+                    water += left_max - height[left]
+                left += 1
+            else:
+                if height[right] >= right_max:
+                    right_max = height[right]
+                else:
+                    water += right_max - height[right]
+                right -= 1
+
+        return water
