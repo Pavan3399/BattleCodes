b702d81ab27f5919e107d04ee2454bbfdb1e77df
Pavan3399 <84954792+Pavan3399@users.noreply.github.com>
2025-07-08 23:55:44 -0400

    Create Insert_Interval.py

---
+++ Insert_Interval.py
@@ -0,0 +1,41 @@
+LeetCode Reuslt:- https://leetcode.com/problems/insert-interval/submissions/1691481159
+
+Example 1:
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+Example 2:
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+
+COde
+_________________________________
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[List[int]]
+        :type newInterval: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        i = 0
+        n = len(intervals)
+
+        # Add all intervals ending before newInterval starts
+        while i < n and intervals[i][1] < newInterval[0]:
+            result.append(intervals[i])
+            i += 1
+        
+        # Merge overlapping intervals
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        result.append(newInterval)
+
+        # Add the remaining intervals
+        while i < n:
+            result.append(intervals[i])
+            i += 1
+        
+        return result
