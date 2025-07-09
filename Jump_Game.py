97f50ee139ae17290997e1d62293c62ce6636cf1
Pavan3399 <84954792+Pavan3399@users.noreply.github.com>
2025-07-08 23:45:17 -0400

    Create Jump_Game.py

---
+++ Jump_Game.py
@@ -0,0 +1,25 @@
+Leetcode Result:- https://leetcode.com/problems/jump-game/submissions/1691474407
+
+Example 1:
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index
+
+Code
+______________________________
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        max_reach = 0
+        for i, num in enumerate(nums):
+            if i > max_reach:
+                return False
+            max_reach = max(max_reach, i + num)
+        return True
